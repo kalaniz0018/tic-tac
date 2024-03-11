1b1fe78950bbf405afeaa2b79d775806c23b074b
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-03-11 15:12:16 -0300

    working whit state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Player = ({ name, symbol }) => {
+  const [isEditing, useIsEditing] = useState(false);
+
+const handleEditClick = () =>{
+    useIsEditing(true)
+}
+
+let playerName = <span className="player-name">{name}</span>
+
+if (isEditing) {
+    playerName = <input type="text" required/>
+}
+
   return (
     <>
       <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+          {playerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>Edit</button>
       </li>
     </>
   );
